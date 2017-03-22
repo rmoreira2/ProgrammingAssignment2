fd155a86f49feed6ebb58fa53b49005238bc6e75
Rodrigo Moreira Carvalho <rmoreira2@yahoo.com>
2017-03-22 18:29:42 -0300

    First code check n with inverse matrix cache

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Those functions creates a matrix with cache functions to store its inverse
+## So evaluating the inverse will first check if its inverse already was stored on the cache
 
-## Write a short comment describing this function
+
+## Receives an invertible matrix
+## returns a list with functions to get / set the matrix and get/set the matrix inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    invCache <- NULL
+    set <- function(y) {
+        x <<- y
+        invCache <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inv) invCache <<- inv
+    getInverse <- function() invCache
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Receives a matrix with list of functions created with makeCacheMatrix
+## Returns the matrix inverse using the solve function. 
+## If the inverse was already evaluated it is cached.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inv <- x$getInverse()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setInverse(inv)
+    inv
 }
